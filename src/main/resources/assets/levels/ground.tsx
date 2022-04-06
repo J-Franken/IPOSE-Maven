7a33d4dd1adcc1b381ebbde98f73873aa0ca441c
fayli <s1136335@student.hsleiden.nl>
2022-04-06 20:41:31 +0200

    Eerste level

---
+++ src/main/resources/assets/levels/ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TX Tileset Ground" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../../Downloads/Pixel Art Platformer - Village Props v2.1.0/Texture/TX Tileset Ground.png" width="512" height="512"/>
+</tileset>
---
+++ src/main/resources/assets/levels/grounddark.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TX Tileset Ground Darken" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../../Downloads/Pixel Art Platformer - Village Props/Texture/TX Tileset Ground Darken.png" width="256" height="256"/>
+</tileset>
