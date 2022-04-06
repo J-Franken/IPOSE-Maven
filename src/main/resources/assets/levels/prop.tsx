62598714782d32637f2c67b77da0de4e3cb0bbf6
fayli <s1136335@student.hsleiden.nl>
2022-04-06 20:47:40 +0200

    Eerste level

--- src/main/resources/assets/levels/prop.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="TX Village Props" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
- <image source="../../Downloads/Pixel Art Platformer - Village Props v2.1.0/Texture/TX Village Props.png" width="1024" height="1024"/>
-</tileset>
